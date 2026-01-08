5f7f5d998ed1cee2b59c7b75121ef5fba634223e
tatianaHG2 <tatianaimb2@gmail.com>
2026-01-07 22:55:53 -0400

    feli y vico

--- proyecto/src/App.tsx
+++ proyecto/src/App.tsx
@@ -5,6 +5,8 @@ import MiaImg from "./assets/Mia.jpeg";
 import PabloImg from "./assets/Pablo.jpeg";
 import ManuelImg from "./assets/Manuel.jpeg";
 import MarizzaImg from "./assets/Marizza.jpeg";
+import VicoImg from "./assets/vico.jpg";
+import FelicitasImg from "./assets/felicitas.jpeg";
 import Modal from "./components/Modal";
 
 type Card = {
@@ -37,21 +39,21 @@ function App() {
         Ataque={100}
         Nombre="Mia Colucci"
         Defensa={50}
-        Descripcion="Manipula a sus enemigos con su mirada"
+        Descripcion="Mia Colucci es la líder carismática del grupo Rebelde Way, una cantante talentosa que lucha por sus ideales y enfrenta conflictos amorosos y familiares en el Elite Way School."
         Imagen={MiaImg}
         Numero={37}
-        Tipo="Cantante de Rock"
-        Debilidad="Dramatica"
+        Tipo="Líder Rebelde"
+        Debilidad="Celosa"
         Rareza="Legendaria"
         onOpen={() =>
           openCard({
             Numero: 37,
             Nombre: "Mia Colucci",
-            Tipo: "Cantante de Rock",
+            Tipo: "Líder Rebelde",
             Ataque: 100,
             Defensa: 50,
-            Descripcion: "Manipula a sus enemigos con su mirada",
-            Debilidad: "Dramatica",
+            Descripcion: "Mia Colucci es la líder carismática del grupo Rebelde Way, una cantante talentosa que lucha por sus ideales y enfrenta conflictos amorosos y familiares en el Elite Way School.",
+            Debilidad: "Celosa",
             Rareza: "Legendaria",
             Imagen: MiaImg,
           })
@@ -62,20 +64,20 @@ function App() {
         Ataque={90}
         Nombre="Marizza Pia Spirito"
         Defensa={80}
-        Descripcion="Desafía la Autoridad y Consigue Aliados"
+        Descripcion="Marizza Pia Spirito es una activista punk apasionada por la justicia social, forma parte de la banda Rebelde Way y desafía las normas establecidas en la escuela."
         Imagen={MarizzaImg}
         Numero={38}
-        Tipo="Activista y Músico Underground"
+        Tipo="Activista Punk"
         Debilidad="Impulsiva"
         Rareza="Épica"
         onOpen={() =>
           openCard({
             Numero: 38,
             Nombre: "Marizza Pia Spirito",
-            Tipo: "Activista y Músico Underground",
+            Tipo: "Activista Punk",
             Ataque: 90,
             Defensa: 80,
-            Descripcion: "Desafía la Autoridad y Consigue Aliados",
+            Descripcion: "Marizza Pia Spirito es una activista punk apasionada por la justicia social, forma parte de la banda Rebelde Way y desafía las normas establecidas en la escuela.",
             Debilidad: "Impulsiva",
             Rareza: "Épica",
             Imagen: MarizzaImg,
@@ -87,21 +89,21 @@ function App() {
         Ataque={120}
         Nombre="Manuel Aguirre"
         Defensa={60}
-        Descripcion="Planea su Venganza en Secreto"
+        Descripcion="Manuel Aguirre, un estudiante becado inteligente y estratega, planea vengarse de aquellos que lo han humillado, mostrando su lado oscuro en el Elite Way School."
         Imagen={ManuelImg}
         Numero={39}
-        Tipo="Estratega y Becado"
-        Debilidad="Sentimental"
+        Tipo="Becado Vengativo"
+        Debilidad="Obsesivo"
         Rareza="Mítica"
         onOpen={() =>
           openCard({
             Numero: 39,
             Nombre: "Manuel Aguirre",
-            Tipo: "Estratega y Becado",
+            Tipo: "Becado Vengativo",
             Ataque: 120,
             Defensa: 60,
-            Descripcion: "Planea su Venganza en Secreto",
-            Debilidad: "Sentimental",
+            Descripcion: "Manuel Aguirre, un estudiante becado inteligente y estratega, planea vengarse de aquellos que lo han humillado, mostrando su lado oscuro en el Elite Way School.",
+            Debilidad: "Obsesivo",
             Rareza: "Mítica",
             Imagen: ManuelImg,
           })
@@ -112,23 +114,73 @@ function App() {
         Ataque={70}
         Nombre="Pablo Bustamante"
         Defensa={90}
-        Descripcion="Controla el Entorno y las Relaciones"
+        Descripcion="Pablo Bustamante, hijo de un político influyente, manipula situaciones para su beneficio, pero lucha con su inseguridad interna en el Elite Way School."
         Imagen={PabloImg}
         Numero={12}
-        Tipo="Político en Formación"
-        Debilidad="Cobarde"
+        Tipo="Político Manipulador"
+        Debilidad="Inseguro"
         Rareza="Rara"
         onOpen={() =>
           openCard({
             Numero: 12,
             Nombre: "Pablo Bustamante",
-            Tipo: "Político en Formación",
+            Tipo: "Político Manipulador",
             Ataque: 70,
             Defensa: 90,
-            Descripcion: "Controla el Entorno y las Relaciones",
-            Debilidad: "Cobarde",
+            Descripcion: "Pablo Bustamante, hijo de un político influyente, manipula situaciones para su beneficio, pero lucha con su inseguridad interna en el Elite Way School.",
+            Debilidad: "Inseguro",
             Rareza: "Rara",
             Imagen: PabloImg,
+          })
+        }
+      />
+
+      <CardDetail
+        Ataque={85}
+        Nombre="Victoria Paz"
+        Defensa={75}
+        Descripcion="Victoria Paz, conocida como Vico C, es una rapera arrogante y talentosa que compite con Mia por el liderazgo del grupo Rebelde Way en la escuela."
+        Imagen={VicoImg}
+        Numero={40}
+        Tipo="Rapera Arrogante"
+        Debilidad="Arrogante"
+        Rareza="Épica"
+        onOpen={() =>
+          openCard({
+            Numero: 40,
+            Nombre: "Vico C",
+            Tipo: "Rapera Arrogante",
+            Ataque: 85,
+            Defensa: 75,
+            Descripcion: "Victoria Paz, conocida como Vico , es una rapera arrogante y talentosa que compite con Mia por el liderazgo del grupo Rebelde Way en la escuela.",
+            Debilidad: "Arrogante",
+            Rareza: "Épica",
+            Imagen: VicoImg,
+          })
+        }
+      />
+
+      <CardDetail
+        Ataque={95}
+        Nombre="Felicitas"
+        Defensa={85}
+        Descripcion="Felicitas es una artista plástica sensible y perfeccionista, que expresa sus emociones a través de sus obras y enfrenta inseguridades en el Elite Way School."
+        Imagen={FelicitasImg}
+        Numero={41}
+        Tipo="Artista Plástica"
+        Debilidad="Insegura"
+        Rareza="Legendaria"
+        onOpen={() =>
+          openCard({
+            Numero: 41,
+            Nombre: "Felicitas",
+            Tipo: "Artista Plástica",
+            Ataque: 95,
+            Defensa: 85,
+            Descripcion: "Felicitas es una artista plástica sensible y perfeccionista, que expresa sus emociones a través de sus obras y enfrenta inseguridades en el Elite Way School.",
+            Debilidad: "Insegura",
+            Rareza: "Legendaria",
+            Imagen: FelicitasImg,
           })
         }
       />
